32d28e6caec89eb0ab0c77267a1437e622e734a9
shera1020 <52871541+shera1020@users.noreply.github.com>
2020-09-11 07:19:17 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        
+        
+        <a href ="/"> React Shooping cart</a>
+    
+      </header>
+      <main>
+        Product list
+      </main>
+      <footer>
+        All right is reserved.
+      </footer>
+    </div>
+  );
+}
+
+export default App;
