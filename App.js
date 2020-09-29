8975bedc1b63cab00624445a6a3608c42a8ac201
shera1020 <52871541+shera1020@users.noreply.github.com>
2020-09-28 21:28:30 -0400

    add cart redux

--- App.js
+++ App.js
@@ -1,22 +1,42 @@
-import React from 'react'
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        
-        
-        <a href ="/"> React Shooping cart</a>
+import React from "react";
+import Products from "./components/Products";
+import Filter from "./components/Filter";
+import Cart from "./components/Cart";
+import store from "./store";
+import { Provider } from "react-redux";
+
+class App extends React.Component {
+  
     
-      </header>
-      <main>
-        Product list
-      </main>
-      <footer>
-        All right is reserved.
-      </footer>
-    </div>
-  );
-}
+  
 
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="grid-container">
+          <header>
+            <a href="/">React Shopping Cart</a>
+          </header>
+          <main>
+            <div className="content">
+              <div className="main">
+                <Filter></Filter>
+                <Products></Products>
+                
+              </div>
+              <div className="sidebar">
+                <Cart
+                  
+                />
+                <Cart />
+              </div>
+            </div>
+          </main>
+          <footer>All right is reserved.</footer>
+        </div>
+      </Provider>
+    );
+  }
+}
 export default App;
